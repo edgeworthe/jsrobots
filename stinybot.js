5a49703e67c0cd88b44f4d5b20313523556afa1f
Eric Edgeworth <ericedge@gmail.com>
2012-03-31 13:15:34 -0700

    adding helper functions
    Mainly refactoring attackSomething() and adjusting wildfire() to
    less frequently attack the wall right behind us.

--- stinybot.js
+++ stinybot.js
@@ -7,10 +7,20 @@ can find.
 
 if( ! this.initialized ) {
     this.alertsOK = 0;
+
     this.locateSelf = function() {
         this.xloc = this.loc_x();
         this.yloc = this.loc_y();
     };
+
+    this.resetTarget = function() {
+        this.targetXY = [-1,-1];
+    };
+
+    this.noTargetLock = function() {
+        return this.targetXY[0] == -1;
+    };
+
     this.checkDamage = function() {
         // potential bug: if run this twice, might get misleading
         // results
@@ -21,10 +31,10 @@ if( ! this.initialized ) {
             this.underAttack = 0;
         this.previousDamage = this.currentDamage;
     };
+
     this.pathFromClosestCorner = function() {
         // TODO: better mathy way to do this?
         // 0-499, 249 = center
-        // TODO: prevent getting hung up in middle
         var angle = rand(90);
         var midpoint = 250;
         if( this.xloc < midpoint ) {
@@ -42,16 +52,22 @@ if( ! this.initialized ) {
         }
         return angle;
     };
+
     this.wildfire = function() {
         // Always Be Firing
         // Downside: Sometimes Be Reloading at inopportune moments
-        // TODO: nearWall() is false even if you're too close, so
-        // readjust to prevent shooting self from wall explosion.
-        if( ( ! this.targetXY[0] || this.targetXY[0] == -1 ) 
-            && ! this.nearWall( this.xloc, this.yloc ) 
-            && this.currentDamage < 40 )
-            this.cannon( this.currentHeading + 135 + rand(90), 100 );
+        angle = this.currentHeading + 135 + rand(90);
+        range = 100;
+        xy = this.polar2cart( this.xloc, this.yloc, angle, range );
+        if( xy[0] < 0 || xy[1] < 0 || xy[0] > 499 || xy[1] > 499 ) {
+            // outside of arena, might hit self in blowback
+            return;
+        }
+        if( this.noTargetLock()
+            && ! this.nearWall( this.xloc, this.yloc ) )
+            this.cannon( angle, range );
     };
+
     this.cart2polar = function( myX, myY, targetX, targetY ) {
         /* Given Cartesian coords return polar adapted
         for the arena coords, relative to current position */
@@ -68,25 +84,38 @@ if( ! this.initialized ) {
         return( Math.round(Math.atan(xdiff/ydiff) 
             * 180/Math.PI + corrective));
     };
+
     this.polar2cart = function( myX, myY, angle, range ) {
         /* Given polar coords relative to current position,
         return likely location of target */
-        // TODO: fix divide-by-zero at modulus 90 (still an issue?)
         var scanRadians = angle * (Math.PI/180);
         var targetX = Math.round(Math.sin(scanRadians) * range + myX);
         var targetY = 
             Math.abs(Math.round(Math.cos(scanRadians) * range - myY));
+        // TODO: mathier way of doing this?
+        if( targetX < 0 ) { targetX = 0; }
+        if( targetX > 499 ) { targetX = 499; }
+        if( targetY < 0 ) { targetY = 0; }
+        if( targetY > 499 ) { targetY = 499; }
         return( [ targetX, targetY ] );
     };
+
     this.nearWall = function( x, y ) {
-        /* Return 1 if we're within close to wall, 0 otherwise */
+        /* Return 1 if we're close to wall, 0 otherwise */
         var retVal = 0;
-        // TODO: closer ok?
-        if( x > 492 || x < 7 || y > 492 || y < 7 ) {
+        var dist = 8;
+        // TODO: need to determine distance to point on the wall
+        // where we'll strike it; right now we just use the
+        // maximum dist to prevent colliding with wall when
+        // roughly perpendicular path, and also causes us to
+        // recalculate travel when we're actually headed *away*
+        // from wall, which could potentially get us killed.
+        if( x > 499-dist || x < dist || y > 499-dist || y < dist ) {
             retVal = 1;
         }
         return( retVal );
     };
+
     this.nearMiddle = function( x, y ) {
         var retVal = 0;
         if( x < 300 && x > 200 && y < 300 && y > 200 )
@@ -94,57 +123,68 @@ if( ! this.initialized ) {
         return( retVal );
     };
 
+    this.trackAndAttackTarget = function( angle, range ) {
+        // TODO: adjust range outward slightly to accommodate
+        // blast radius, target movement, and scan inaccuracy?
+        // TODO: consider altering this.currentHeading such
+        // that we're travelling at 90 degrees to the target,
+        // effectively circling them at range.  Problems with
+        // this: 1) might end up hitting wall, 2) need to
+        // stop and then take new heading, else we just grind
+        // to a halt.
+        var haveFired = 0;
+        if( range <= 350 ) {
+            haveFired = this.cannon( angle, range );
+        } 
+        this.targetXY = this.polar2cart( this.xloc, this.yloc,
+            angle, range );
+        this.targetFound = 2;
+        if( this.alertsOK && !this.alerted ) {
+            alert( ["FOUND TARGET: targetX,Y, scandir, range, mylocx,y: ",
+                this.targetXY, angle, range, this.xloc, this.yloc ] );
+            this.alerted = 1;
+        }
+        return haveFired;
+    };
+
+    this.adjustScan = function() {
+        if( this.alertsOK && this.alerted ) {
+            alert( ["LOST TARGET: targetX,Y, scandir, range, mylocx,y: ", 
+                this.targetXY, this.scanDirection, targetRange, 
+                this.xloc, this.yloc ] );
+            this.alerted = 0;
+        }
+        if( ! this.targetFound ) {
+            // TODO: adjust scanDirection based on our
+            // current heading?  Probably want to pick a
+            // direction that grabs us the widest swath of
+            // the playing field, yet not ignore corners,
+            // either.
+            this.scanDirection += 10;
+            while( this.scanDirection >= 360 )
+                this.scanDirection -= 360;
+            this.resetTarget();
+        } else {
+            this.targetFound -= 1;
+        }
+    };
+
     this.attackSomething = function() {
         // TODO: stop firing in your own path, dimwit
+        // TODO: long function, consider breaking up into smaller
+        // chunks, e.g. scan/fire/alert
         var haveFired = 0;
-        if( this.targetXY[0] && this.targetXY[0] != -1 ) {
+        if( this.targetXY[0] && ! this.noTargetLock() ) {
             this.scanDirection = this.cart2polar( this.xloc, this.yloc,
                 this.targetXY[0], this.targetXY[1] );
         }
         // TODO: adjust resolution if we have a known target?
         var targetRange = this.scan( this.scanDirection, 10 );
         if( targetRange ) {
-            // TODO: adjust range outward slightly to accommodate
-            // blast radius, target movement, and scan inaccuracy?
-            // TODO: consider altering this.currentHeading such
-            // that we're travelling at 90 degrees to the target,
-            // effectively circling them at range.  Problems with
-            // this: 1) might end up hitting wall, 2) need to
-            // stop and then take new heading, else we just grind
-            // to a halt.
-            if( targetRange <= 350 ) {
-                haveFired = this.cannon( this.scanDirection, targetRange );
-            } 
-            this.targetXY = this.polar2cart( this.xloc, this.yloc,
-                this.scanDirection, targetRange );
-            // TODO: if values are outside the walls, adjust inward
-            this.targetFound = 2;
-            if( this.alertsOK && !this.alerted ) {
-                alert( ["FOUND TARGET: targetX,Y, scandir, range, mylocx,y: ",
-                    this.targetXY, this.scanDirection, targetRange,
-                    this.xloc, this.yloc ] );
-                this.alerted = 1;
-            }
+            haveFired = 
+                this.trackAndAttackTarget( this.scanDirection, targetRange );
         } else {
-            if( this.alertsOK && this.alerted ) {
-                alert( ["LOST TARGET: targetX,Y, scandir, range, mylocx,y: ", 
-                    this.targetXY, this.scanDirection, targetRange, 
-                    this.xloc, this.yloc ] );
-                this.alerted = 0;
-            }
-            if( ! this.targetFound ) {
-                // TODO: adjust scanDirection based on our 
-                // current heading?  Probably want to pick
-                // a direction that grabs us the widest
-                // swath of the playing field, yet not ignore
-                // corners, either.
-                this.scanDirection += 10;
-                while( this.scanDirection >= 360 )
-                    this.scanDirection -= 360;
-                this.targetXY = [-1,-1];
-            } else {
-                this.targetFound -= 1;
-            }
+            this.adjustScan();
         }
         if( ! haveFired )
             this.wildfire();
@@ -165,6 +205,7 @@ if( ! this.initialized ) {
             this.haveDriven = 1;
         }
     };
+
     this.moveSpiral = function() {
         /* move in spiral shape */
         // TODO: move to corner upon initialization/spiral finish
@@ -172,11 +213,13 @@ if( ! this.initialized ) {
         // TODO: once get close to middle, reset to random corner
         // and start over
     };
+
     this.moveCircle = function() {
         /* trace a circle within bounds of arena */
         // TODO: move in a circle and change circle size after a
         // certain number of revolutions.
     };
+
     this.yellowWallpaper = function() {
         /* move around perimeter, very close to walls */
         if( ! this.nearWall( this.xloc, this.yloc ) ) {
@@ -186,11 +229,12 @@ if( ! this.initialized ) {
     };
 
 
+    // TODO: stop calling multiple times on first iteration?
     this.locateSelf();
 
     this.drivePower = 100;
     this.haveDriven = 0; // incrementing boolean. hacky.
-    this.targetXY = [-1,-1];
+    this.resetTarget();
     this.targetFound = 0;
 
     this.scanDirection = this.pathFromClosestCorner();
@@ -210,9 +254,13 @@ if( ! this.initialized ) {
 // nearWall, nearCenter, outsideWalls, etc.
 // TODO: a lot of dmg taken from robot collisions; consider
 // hugging walls until take damage, then move to another wall.
+// TODO: Seems we do okay against 1-2 of any other built-in bots,
+// but when we get to 3 the perimeter guard or mosquito bot kills
+// me well because it's often in my path and does poorly killing
+// itself.  Any way to handle that?  (One way: stop shooting
+// yourself at walls!)
 
 this.locateSelf();
-// TODO: no dmg check presently?
 this.checkDamage();
 this.attackSomething();
 if( this.speed() == 0 && this.haveDriven != 0 || this.underAttack ) {
