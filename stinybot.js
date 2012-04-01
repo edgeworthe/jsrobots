22c1b9e1668f682d5b9b666a24b8743fb4ec2d18
Eric Edgeworth <ericedge@gmail.com>
2012-03-31 20:16:35 -0700

    cleaning up, documenting
    Removing bots and functions not currently used and adding
    function documentation.

--- stinybot.js
+++ stinybot.js
@@ -5,10 +5,12 @@ Drive around randomly in the middle and shoot at what you
 can find.
 */
 
-if( ! this.initialized ) {
-    this.alertsOK = 0;
+if( ! this.iterations ) {
+    this.alertsOK = 0; // sometimes alert() helps debugging
 
     this.locateSelf = function() {
+        /* record our x,y location to avoid multiple calls to
+           loc_x() and loc_y() in a single iteration */
         this.xloc = this.loc_x();
         this.yloc = this.loc_y();
     };
@@ -19,29 +21,36 @@ if( ! this.initialized ) {
     // should be able to) and/or movement strategies (always in
     // same places, etc.)
     this.resetTarget = function() {
+        /* place target outside arena walls to represent our lack
+           of target lock */
         this.targetXY = [-1,-1];
     };
 
     this.haveTargetLock = function() {
+        /* Whether we've recorded our target's x,y coords
+           recently; see also this.resetTarget() */
         return this.targetXY[0] != -1;
     };
 
     this.checkDamage = function() {
-        // potential bug: if run this twice, might get misleading
-        // results
-        this.currentDamage = this.damage();
-        if( this.currentDamage != this.previousDamage )
-            this.underAttack = 10;
-        else if( this.underAttack > 0 )
-            this.underAttack -= 1;
-        this.previousDamage = this.currentDamage;
+        /* Keep track of last-reported damage level and set
+           this.underAttack if it's changed since last iteration */
+        // avoid running more than once per iteration
+        if( this.checkDamageIteration != this.iterations ) {
+            this.currentDamage = this.damage();
+            if( this.currentDamage != this.previousDamage )
+                this.underAttack = 10;
+            else if( this.underAttack > 0 )
+                this.underAttack -= 1;
+            this.previousDamage = this.currentDamage;
+            this.checkDamageIteration = this.iterations;
+        }
     };
 
     this.whichQuadrant = function( x, y ) {
         /* given x,y coords, return which quadrant those coords
-        are in, where the quadrants are set up as:
-        4 1
-        3 2 */
+        are in, where the quadrants are set up as: 4 1
+                                                   3 2 */
         var midpoint = 250;
         var quadrant = 0;
         if( x > midpoint ) {
@@ -75,12 +84,14 @@ if( ! this.initialized ) {
         return angle;
     };
     this.isOutsideArena = function( x, y ) {
+        /* given x,y coords, return boolean whether it's outside
+           (or at) the arena walls */
         // TODO: why does y<=0 here cause us to run against wall?
         return( x <= 0 || x >= 499 || y <= 10 || y >= 499 );
     };
 
     this.wildfire = function() {
-        // Always Be Firing
+        /* Always Be Firing */
         // Downside: Sometimes Be Reloading at inopportune moments
         var minXY = 10;
         var maxXY = 499 - minXY;
@@ -145,22 +156,10 @@ if( ! this.initialized ) {
         return( retVal );
     };
 
-    this.nearMiddle = function( x, y ) {
-        var retVal = 0;
-        if( x < 300 && x > 200 && y < 300 && y > 200 )
-            retVal = 1;
-        return( retVal );
-    };
-
     this.trackAndAttackTarget = function( angle, range ) {
+        /* logic for firing on and recording target position */
         // TODO: adjust range outward slightly to accommodate
         // blast radius, target movement, and scan inaccuracy?
-        // TODO: consider altering this.currentHeading such
-        // that we're travelling at 90 degrees to the target,
-        // effectively circling them at range.  Problems with
-        // this: 1) might end up hitting wall, 2) need to
-        // stop and then take new heading, else we just grind
-        // to a halt.
         var haveFired = 0;
         var minRange = 250 - this.iterations/100;
         if( minRange < 50 )
@@ -168,19 +167,18 @@ if( ! this.initialized ) {
         if( range <= 350 ) {
             haveFired = this.cannon( angle, range );
         } 
+        // based on range to target, run perpendicular to them
+        // with adjustments to close or increase distance as
+        // appropriate.
         if( range <= 25 ) {
-            // edge away if we're too close in
             this.circleHeading = this.circleHeading < 0 ? -135 : 135;
         } else if( range <= minRange ) {
-            // move perpendicularly if we're within a good range
             this.circleHeading = this.circleHeading < 0 ? -90 : 90;
         } else {
-            // move in closer if we're too far away
             this.circleHeading = this.circleHeading < 0 ? -45 : 45;
         }
         this.targetXY = 
             this.polar2cart( this.xloc, this.yloc, angle, range );
-        this.targetFound = 2;
         if( this.alertsOK && !this.alerted ) {
             alert( ["FOUND TARGET: targetX,Y, scandir, range, mylocx,y: ",
                 this.targetXY, angle, range, this.xloc, this.yloc ] );
@@ -190,6 +188,7 @@ if( ! this.initialized ) {
     };
 
     this.adjustScan = function() {
+        /* adjust this.scanDirection based on target status */
         if( this.alertsOK && this.alerted ) {
             alert( ["LOST TARGET: targetX,Y, scandir, range, mylocx,y: ", 
                 this.targetXY, this.scanDirection, targetRange, 
@@ -212,7 +211,7 @@ if( ! this.initialized ) {
     };
 
     this.attackSomething = function() {
-        // TODO: stop firing in your own path, dimwit
+        /* find a target or shoot wildly */
         var haveFired = 0;
         if( this.targetXY[0] && this.haveTargetLock() ) {
             this.scanDirection = this.cart2polar( this.xloc, this.yloc,
@@ -223,6 +222,9 @@ if( ! this.initialized ) {
         if( targetRange ) {
             haveFired = 
                 this.trackAndAttackTarget( this.scanDirection, targetRange );
+            // TODO: adjust the targetFound logic--might be
+            // causing us to miss fast-moving targets presently.
+            this.targetFound = 2;
         } else {
             this.adjustScan();
         }
@@ -230,22 +232,20 @@ if( ! this.initialized ) {
             this.wildfire();
     };
 
-    this.moveAroundRandomly = function() {
-        // TODO: Consider more logic for avoiding other bots;
-        // we lose a lot of battles just from running into/over
-        // others. Not moving seems to be a good advantage
-        // against some bots.  Hm.
+    this.keepOnTrucking = function() {
+        /* Pretty much all movement logic herein.  Basically:
+           keep moving to a different quadrant until you find a
+           target; once you find a target, circle them. */
         // TODO: consider retreating to corner when dmg reaches
-        // certain point to avoid Mosquitoes
+        // certain point to avoid Mosquitoes?
         var initialHeading = this.currentHeading;
         if( this.haveTargetLock() && ! this.underAttack ) {
-            // TODO: fix case where I'm at 0,250 and target is at
-            // 0,0 -- we get hung up currently there.
             var circlingTarget = this.scanDirection + this.circleHeading;
             if( Math.abs(circlingTarget - this.currentHeading) > 10 )
                 this.currentHeading = circlingTarget;
         } 
-        // TODO: is 20 how far we travel in one iteration?
+        // TODO: how far do we travel in one iteration at 100%
+        // drive?
         var destination = this.polar2cart( this.xloc, this.yloc, 
             this.currentHeading, 25 );
         if( this.isOutsideArena( destination[0], destination[1] ) ) {
@@ -259,30 +259,6 @@ if( ! this.initialized ) {
         }
     };
 
-    this.moveSpiral = function() {
-        /* move in spiral shape */
-        // TODO: move to corner upon initialization/spiral finish
-        // TODO: randomly determine spiraling direction
-        // TODO: once get close to middle, reset to random corner
-        // and start over
-    };
-
-    this.moveCircle = function() {
-        /* trace a circle within bounds of arena */
-        // TODO: move in a circle and change circle size after a
-        // certain number of revolutions.
-    };
-
-    this.yellowWallpaper = function() {
-        /* move around perimeter, very close to walls */
-        if( ! this.nearWall( this.xloc, this.yloc ) ) {
-            // TODO: move to a wall
-        } 
-        // TODO: move along walls
-    };
-
-
-    // TODO: stop calling multiple times on first iteration?
     this.locateSelf();
 
     this.drivePower = 100;
@@ -293,8 +269,6 @@ if( ! this.initialized ) {
     this.currentHeading = this.pathFromClosestCorner();
     this.circleHeading = 90;
     this.iterations = 0;
-
-    this.initialized = 1;
 }
 
 // TODO: add logic for switching "modes"--e.g.:
@@ -306,17 +280,14 @@ if( ! this.initialized ) {
 // - differentiate between collision damage and cannon damage?
 // TODO: consider adding an arena object for methods like
 // nearWall, nearCenter, outsideWalls, etc.
-// TODO: a lot of dmg taken from robot collisions; consider
-// hugging walls until take damage, then move to another wall.
 // TODO: Seems we do okay against 1-2 of any other built-in bots,
 // but when we get to 3 the perimeter guard or mosquito bot kills
 // me well because it's often in my path and does poorly killing
-// itself.  Any way to handle that?  (One way: stop shooting
-// yourself at walls!)
+// itself.  Any way to handle that? 
 
-// TODO: change behavior based on iterations?
 this.iterations += 1;
-this.locateSelf();
+if( this.iterations > 1 )
+    this.locateSelf();
 this.checkDamage();
 this.attackSomething();
 if( this.speed() == 0 ) {
@@ -324,12 +295,7 @@ if( this.speed() == 0 ) {
     // twice in a single iteration like all the sample bots do.
     // TODO: figure out some way to stop cheating that still
     // prevents getting trampled by Mosquito
-    // TODO: sometimes get hung up after collision with
-    // tower/twin in center due to moveAroundRandomly
-    // logic--instead flee to corner?
-    // TODO: potential bug where we get hung up on bot if collide
-    // near wall?  Hard to replicate, unfortunately.
     this.drive(this.currentHeading, 0);
 }
-this.moveAroundRandomly();
+this.keepOnTrucking();
 
